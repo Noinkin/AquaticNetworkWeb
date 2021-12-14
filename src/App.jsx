848e053d958fa45c4b8cc9b379e9591204b89577
Noinkin <87577662+Noinkin@users.noreply.github.com>
2021-12-14 17:08:04 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+    	</script>
+    	<div class="navbar">
+    		<a class="activeNav">Home</a>
+    		<a href="https://pangeastudios.repl.co/news/news.html">News</a>
+    		<a href="https://pangeastudios.repl.co/store/store_home.html">Store</a>
+    		<a href="https://github.com/Pangea-Studios/">Github</a>
+    		<div class="dropdown">
+    			<button class="dropbtn">Games</button>
+                <div class="dropdown-content">
+                    <a href="https://pangeastudios.repl.co/games/minecraft/home.html">Minecraft</a>
+                    <a href="https://pangeastudios.repl.co/games/roblox/home.html">Roblox</a> 
+                </div>
+            </div> 
+          </div>
+      <div class="content">
+            <div class="top">
+
+            </div>
+          </div>
+    </main>
+  );
+}
+
+export default App;
