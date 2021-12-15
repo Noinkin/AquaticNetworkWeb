92ad2b12e51f1ad2dd190b4fdf48d2d075f14de2
Noinkin <87577662+Noinkin@users.noreply.github.com>
2021-12-15 19:25:42 +0000

    Add more stuff

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,67 @@ import './App.css';
 function App() {
   return (
     <main>
-    	</script>
     	<div class="navbar">
     		<a class="activeNav">Home</a>
-    		<a href="https://pangeastudios.repl.co/news/news.html">News</a>
-    		<a href="https://pangeastudios.repl.co/store/store_home.html">Store</a>
-    		<a href="https://github.com/Pangea-Studios/">Github</a>
+    		<a href="https://aquaticnetwork.repl.co/updates/updates.html">Updates</a>
+    		<a href="https://aquaticnetwork.repl.co/store/store.html">Store</a>
+            <a href="https://aquaticnetwork.repl.co/server/ip.html">Server IP</a>
+            <a href="https://discord.gg/SEtZtHEBss">Discord</a>
     		<div class="dropdown">
-    			<button class="dropbtn">Games</button>
+    			<button class="dropbtn">Other</button>
                 <div class="dropdown-content">
-                    <a href="https://pangeastudios.repl.co/games/minecraft/home.html">Minecraft</a>
-                    <a href="https://pangeastudios.repl.co/games/roblox/home.html">Roblox</a> 
+                    <a href="https://aquaticnetwork.repl.co/vote/vote.html">Voting</a>
+                    <a href="https://aquaticnetwork.repl.co/news/news.html">News</a> 
                 </div>
             </div> 
-          </div>
-      <div class="content">
-            <div class="top">
-
-            </div>
-          </div>
+        </div>
+    <div class="content">
+        <div class="top">
+            Aquatic Network is a Minecraft Bedrock Edition Server. It is currently in the development phase, we hope to release it as soon as possible.
+            <br>
+            </br>
+            <br>
+            </br>
+            Backstory
+        </div>
+        <br>
+        </br>
+        <div class="essay">
+            Aquatic Network started off as Corrupted Network, the name change was decided within a year after it was started. Aquatic Network, formerly known as Corrupted Network, used to have a realm but now it has a server after decisions made by staff. Aquatic started off after the decision to make it, at the time the members of staff were also staff for another server, this got the co owner of the other server angry. Not too long after this they were banned off the other server and have hated them ever since. They have been banned off the server many times. After they were banned, they carried on to upgrade the realm then server more and more until the state it is in today.
+        </div>
+        <br>
+        </br>
+        <div class="top">
+            Staff Team
+        </div>
+        <div class="staffteam">
+            Owner: DemonWolfGirl86
+            <br>
+            </br>
+            Co Owner: BrineWolf1
+            <br>
+            </br>
+            Staff Managers: Glitchy6964, Bondns1yt
+            <br>
+            </br>
+            Head Admins: DARKLORD98989, StinkySisi
+            <br>
+            </br>
+            Developers: PackagedMango4(Noinkin), Enjoy Ben, TheAwesomeGuy47
+            <br>
+            </br>
+            Builders: Sam B Gaming9, randomname46
+            <br>
+            </br>
+            Admins: AA68, IchiyaTheMan, KawaiiWolf13917
+            <br>
+            </br>
+            Mods: KendallFowler, vEternal-Atakev
+            <br>
+            </br>
+            Trial Mods: AnemicDragon726, Eliii, mamabambi, EpicMorgan, ShrEKzwAMP
+        </div>
+    </div>
     </main>
   );
 }
